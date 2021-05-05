5d9bae12e407d04cf435d1e4ccee01494928bafa
CaronlineArg <33405220+CaronlineArg@users.noreply.github.com>
2021-05-05 19:44:39 -0300

    Add files via upload

---
+++ calculadora-main/calculadora-main/scriptCaroSCalc.js
@@ -0,0 +1,34 @@
+
+const historialCalcu = document.querySelector('#historial-pantalla')
+
+//a la variable local pantalla le asociamos el selectorquery pantalla
+let pantalla = document.querySelector("#resultado-pantalla")
+
+//creamos la funcion getData(red) para obtener por parametro el valor
+//definimos como variable local al valor que sera enviado x parametro como el valor ref
+//definimos la variable valor, la concatenamos y se accede por pantalla
+function getData(ref){
+    let value = ref.value;
+    pantalla.value += value;
+    {
+        historialCalcu.value = pantalla.value +"="+ eval(pantalla.value) ;
+        }
+}
+//igualamos el valor de limpiar a 0
+function limpiar(){
+    pantalla.value = '';
+}
+
+function resultado(){
+    try{
+        pantalla.value = eval(pantalla.value)
+        
+    }catch(error){
+        pantalla.value='ERROR';
+        setTimeout(() => {
+            limpiar()
+        }, 500);
+    }
+}
+
+
